5671ef938b21f0d4b690d5422e14adcf256ab14a
hhshhhskk <hhshhhskk@naver.com>
2022-08-07 17:23:05 +0900

    코드 리팩토링 - API

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,9 +1,7 @@
 import { useLocation } from "react-router";
 import { useQuery } from "react-query";
 import styled from "styled-components";
-
-// 롤 API 키는 발급 시 24시간 사용가능
-const API_KEY = "RGAPI-f9039980-b13f-43f0-9c09-8f499e8737cd";
+import { getSummonerId, getSummonerData } from '../api'
 
 const Wrapper = styled.div`
   display: flex;
@@ -36,49 +34,21 @@ const SummonerName = styled.div`
     color: black;
 `;
 
-// interface ISummonerInfo {
-//     leagueId: string,
-//     queueType: string,
-//     tier: string,
-//     rank: string,
-//     summonerId: string,
-//     summonerName: string,
-//     leaguePoints: number,
-//     wins: number,
-//     losses: number,
-//     veteran: boolean,
-//     inactive: boolean,
-//     freshBlood: boolean,
-//     hotStreak: boolean,
-// }
-
 function Search() {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const summonerName = searchParams.get("keyword");
-    const { data: summonerIdData } = useQuery(["lol", "summoner", "v4", "summoners", "by-name"], getSummonerId);
+    const { data: summonerIdData } = useQuery(["lol", "summoner", "v4", "summoners", "by-name"], () => getSummonerId(summonerName), {
+        enabled: !!summonerName,
+    });
     console.log(summonerIdData);
-
-    const { data: summonerInfoData } = useQuery(["lol", "league", "v4", "entries", "by-summoner"], async () => await getSummonerData());
-
-
-    // 입력한 소환사 이름으로 소환사 ID값 API호출
-    async function getSummonerId() {
-        return await fetch(
-            `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`
-        )
-            .then((response) => response.json())
-    }
-    //임시 
-    // const summonerId = "eSXPZHMS9NP7hnsupEmoRYJtfzuDOTGABYKffRqqGA5p9g";
     const summonerId = summonerIdData?.id;
-    function getSummonerData() {
-        console.log(summonerId);
-        return fetch(
-            `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`
-        )
-            .then((response) => response.json())
-    }
+    const { data: summonerInfoData } = useQuery(["lol", "league", "v4", "entries", "by-summoner"], () => getSummonerData(summonerId),
+        {
+            enabled: !!summonerId,
+        }
+    );
+
     return (
 
         <Wrapper>
--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,5 @@
 // 롤 API 키는 발급 시 24시간 사용가능
-// const API_KEY = "RGAPI-63b794ed-8beb-4929-98cd-a617cbd791c8";
-
-//임시
-// const summonerName = "hide on bush";
+const API_KEY = "RGAPI-9896f12e-3de9-46c6-9471-e0823f14066a";
 
 // export interface IgetSummonerId {
 //   id: string;
@@ -13,5 +10,37 @@
 //   revisionDate: number;
 //   summonerLevel: number;
 // }
+
+// interface ISummonerInfo {
+//     leagueId: string,
+//     queueType: string,
+//     tier: string,
+//     rank: string,
+//     summonerId: string,
+//     summonerName: string,
+//     leaguePoints: number,
+//     wins: number,
+//     losses: number,
+//     veteran: boolean,
+//     inactive: boolean,
+//     freshBlood: boolean,
+//     hotStreak: boolean,
+// }
+
+// 입력한 소환사 이름으로 소환사 ID값 API호출
+export function getSummonerId(summonerName: any) {
+    return fetch(
+        `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`
+    )
+        .then((response) => response.json())
+}
+// getSummonerId에서 가져온 Id로 소환사 데이터 API호출
+export function getSummonerData(summonerId: string) {
+    console.log(summonerId);
+    return fetch(
+        `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${API_KEY}`
+    )
+        .then((response) => response.json())
+}
 
 export async function getSummoerName() {}
